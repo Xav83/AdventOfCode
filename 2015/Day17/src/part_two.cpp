4b71c1dd6c8c9c296ac8812fb33631cc3508a084
Xav83 <x.jouvenot@gmail.com>
2019-12-03 12:30:44 +0100

    Adds the solution of the second part of the day 17

---
+++ 2015/Day17/src/part_two.cpp
@@ -0,0 +1,74 @@
+#include "string_manipulation.hpp"
+#include <cassert>
+#include <fstream>
+#include <iostream>
+#include <limits>
+#include <vector>
+
+using Container = size_t;
+using Containers = std::vector<Container>;
+
+size_t countNumberOfCombination(const Containers &containers, size_t volum,
+                                size_t numberOfContainerUsed,
+                                size_t &numberMinOfContainerUsed) {
+  size_t numberOfCombination{0};
+  for (auto index = size_t{0}; index < containers.size(); ++index) {
+    const int remainingVolum = volum - containers[index];
+    if (remainingVolum == 0) {
+      if (numberMinOfContainerUsed > (numberOfContainerUsed + 1)) {
+        numberOfCombination = 1;
+        numberMinOfContainerUsed = numberOfContainerUsed + 1;
+      } else if (numberMinOfContainerUsed == (numberOfContainerUsed + 1)) {
+        ++numberOfCombination;
+      } else {
+        std::cout << "BOUM " << std::endl;
+      }
+      continue;
+    }
+    if (remainingVolum < 0) {
+      continue;
+    }
+    if ((numberOfContainerUsed + 1) >= numberMinOfContainerUsed) {
+      continue;
+    }
+    Containers containersNotUsed;
+    std::copy(containers.begin() + index + 1, containers.end(),
+              std::back_inserter(containersNotUsed));
+
+    numberOfCombination += countNumberOfCombination(
+        containersNotUsed, remainingVolum, numberOfContainerUsed + 1,
+        numberMinOfContainerUsed);
+  }
+  return numberOfCombination;
+}
+
+int main(int argc, char **argv) {
+  assert(argc == 4);
+
+  std::ifstream file(argv[1]);
+  assert(file.is_open());
+
+  const auto totalVolumeOfEggnog = atoi(argv[2]);
+  const auto expectedResult = atoi(argv[3]);
+
+  std::string fileContent((std::istreambuf_iterator<char>(file)),
+                          std::istreambuf_iterator<char>());
+
+  Containers containers;
+
+  foreachLineIn(fileContent, [&containers](const std::string &line) {
+    containers.emplace_back(atoi(line.c_str()));
+  });
+
+  auto numberMinOfContainerUsed = containers.size();
+  const auto numberOfCombinations = countNumberOfCombination(
+      containers, totalVolumeOfEggnog, 0, numberMinOfContainerUsed);
+
+  if (expectedResult == numberOfCombinations) {
+    return 0;
+  } else {
+    std::cout << "The result found is " << numberOfCombinations
+              << " but the expected result is " << expectedResult << std::endl;
+    return 1;
+  }
+}
